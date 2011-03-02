aaf326d153f325cd9ceb34aec988b33e25dca410
Jesse Kriss <jesse@jklabs.net>
2011-03-01 19:19:23 -0500

    showing other cities, too

--- fiveoclock.rb
+++ fiveoclock.rb
@@ -18,11 +18,19 @@ not_found do
   redirect '/'
 end
 
+helpers do
+  def place_link(country_tz)
+    name = country_tz.timezone.friendly_identifier(true)
+    link = "<a href=http://maps.google.com/maps?z=5&t=p&q=#{country_tz.latitude.to_f},#{country_tz.longitude.to_f}+(#{CGI.escape(name)})>#{name}</a>"
+  end
+end
+
 get '/' do
   @time = Time.now.utc
   @time += params[:offset].to_i if params[:offset]
   
-  @fiveoclockhere = settings.zones.find { |tz| tz.timezone.period_for_utc(@time).to_local(@time).hour == 5 + 12 }
+  @other_locations = settings.zones.find_all { |tz| tz.timezone.period_for_utc(@time).to_local(@time).hour == 5 + 12 }
+  @fiveoclockhere = @other_locations.delete_at(0)
   # @fiveoclockhere = TZInfo::Timezone.get(@fiveoclockhere.identifier)
   @location = @fiveoclockhere.identifier.split('/').last.gsub("_"," ")
   @afterfive = @fiveoclockhere.timezone.period_for_utc(@time).to_local(@time)
